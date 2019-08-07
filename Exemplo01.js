38b73c24e344929915253c0bc211ca65e6a90a3c
Henrique Linhares Montibeller <iquemontibeller@gmail.com>
2019-08-07 08:38:06 -0300

    Finalizado os exemplos de ajax e jQuery

--- Exemplo01.js
+++ Exemplo01.js
@@ -2,17 +2,42 @@ $(function () {
 
     //var nome = '';
     $nome = '';
+    // $('#cep').on('blur', function(){
+    //     buscarCEP();
+    // })
+    $('#cep').on('keypress', function(evt){
+        if(evt.keyCode == 13){
+            buscarCEP()
+        }
+    })
 
+  function buscarCEP(){
+    //document.getElementById("cep").value
+    $cep = $('#cep').val().replace('-','');
+
+    if($cep.length != 8){
+        alert('Tamanho do cep invalido');
+        $("#cep").focus();
+        return;
+    }
+    console.log($cep);
     $.ajax({
-        url: 'https://viacep.com.br/ws/01001000/json/',
+        url: 'https://viacep.com.br/ws/'+$cep+'/json/',
         method: 'get',
         success: function (data) {
             $localidade = data.localidade;
             $logradouro = data.logradouro;
+            $unidadeFederativa = data.uf;
 
             $("#localidade").val($localidade);
             $("#logradouro").val($logradouro);
+            $("#unidade-federativa").val($unidadeFederativa);
+            $("#numero").focus();
          },
-        error: function (erro) { }
+        error: function (erro) { 
+            alert('CEP invalido');
+            $("#cep").focus();
+        }
     });
+  }
 });
